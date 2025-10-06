8914c755c1dd7e618e32f46300637772974ddba3
Gbekeloluwa Jones <gbekeloluwa.jones@pau.edu.ng>
2025-10-06 09:32:58 +0100

    (My week 2 practices)

---
+++ week-2/practice_2.rs
@@ -0,0 +1,12 @@
+fn main(){
+
+	println!();// prints just a 
+
+	println!("hello");//prints
+
+	println!("format {} argumets","some"); //prints format some arguments 
+
+	println!("My name is {}. I am the class rep pf stream 1","Dagogo William-Jumbo");
+
+	
+}
