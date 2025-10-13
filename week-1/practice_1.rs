e9b8f35d876d26ae4ce64a6a117e74ecca68aee0
Gbekeloluwa Jones <gbekeloluwa.jones@pau.edu.ng>
2025-10-13 09:59:35 +0100

    (my week 3 practices)

---
+++ week-1/practice_1.rs
@@ -0,0 +1,3 @@
+fn main() {
+	println!("Welcome to COS1O1!");
+}
---
+++ week-2/practice_1.rs
@@ -0,0 +1,13 @@
+fn main(){
+	 println!("Welcome to COS 101");
+
+      println!("The course Learning outcome is as follows");
+
+      println!("1. Distinguishes between computer and computer programming,/n
+                2. Develop some techniques in computer science,/n
+                3. Understand the different areas of study in computer science 
+                4. Conversant with applicastions of computer science 
+                5. Navigate the career prospects in computer science 
+                6. Coonversant with computer programming prospects ");
+
+  }
