dc12883e67c4bd2bf3e551a5e7538752522d3a0a
Gbekeloluwa Jones <gbekeloluwa.jones@pau.edu.ng>
2025-10-20 05:40:01 +0100

    (My practice 5 week 4)

--- week_4/practice_5/src/main.rs
+++ week_4/practice_5/src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+//Rust program to read height of a person
+// and them print if person is tall, dwarf
+// or average height person
+
+use std::io;
+
+fn main() 
+{
+    let mut input = String::new();
+     
+     println!("\nEnter Your height (in centimetres):");
+    io::stdin().read_line(&mut input).expect("Not a valid string");
+    let height:f32 = input.trim().parse().expect("Not a valid number");
+
+    if height >= 150.0 && height <= 170.0
+    {
+        println!("You are of average height person");
+    } 
+     else if height > 170.0 && height<= 195.0
+     {
+        println!("You are tall");
+     }
+      else if height < 150.0 && height > 100.0
+     {
+        println!("You are dwarf");
+     }
+     else 
+     {
+        println!("Abnormal height");
+     }
 }
+
+
