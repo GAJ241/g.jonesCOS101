3bcc79281468dfaaf9b2ab70b313749f73bca54f
Gbekeloluwa Jones <gbekeloluwa.jones@pau.edu.ng>
2025-12-01 00:40:32 +0100

    (my week 10 practice 8)

---
+++ week-10/practice_8/target/debug/deps/practice_8.d
@@ -0,0 +1,5 @@
+C:\Users\gbeke\OneDrive\Документы\g.jonesCOS101\week-10\practice_8\target\debug\deps\practice_8.d: src\main.rs
+
+C:\Users\gbeke\OneDrive\Документы\g.jonesCOS101\week-10\practice_8\target\debug\deps\practice_8.exe: src\main.rs
+
+src\main.rs:
